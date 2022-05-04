5c831ecb3873d0324bdd8cc17f39c5833f644e88
Numan Erter <numan.erter@student.htldornbirn.at>
2022-05-04 13:30:54 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { FooterContainer } from "./containers/footer";
+import Index from "./components/header/Index";
 
 function App() {
   return (
     <>
       <FooterContainer />
+      <Index />
     </>
   );
 }

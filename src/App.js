00ebbe9276f6d23a215b62f578e7350d677b472f
metehan <metehan.altundal@student.htldornbirn.at>
2022-05-04 15:14:51 +0200

    og sauerstoff

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { FooterContainer } from "./containers/footer";
 import Header from "./components/header/Header";
 import Card from "./components/Cards/Card";
+import SauerstoffCard from "./components/Sauerstoff/SauerstoffCard";
 
 function App() {
   return (
     <>
       <Header />
+      <SauerstoffCard />
       <Card />
       <FooterContainer />
     </>

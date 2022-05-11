896dd6d45c8c31ec4c430738e612f0f7c01cbe6d
Numan Erter <numan.erter@student.htldornbirn.at>
2022-05-11 13:23:42 +0200

    <ikfjK>Jf<ksdjfsdf

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import { FooterContainer } from "./containers/footer";
 import Header from "./components/header/Header";
 import Card from "./components/Cards/Card";
-import SauerstoffCard from "./components/Sauerstoff/SauerstoffCard";
-import Lautstaerke from "./components/Lautstaerke/Lautstaerke";
 
 function App() {
   return (
     <>
       <Header />
-      <SauerstoffCard />
       <Card />
-      <Lautstaerke />
       <FooterContainer />
     </>
   );
--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -3,19 +3,47 @@ import "./CardStyle.css";
 
 function Card() {
   return (
-    <div className="wrapper2">
-      <CardApp />
+    <div className="wrapper">
+      <SauerstoffApp />
+      <LautstaerkeApp />
+      <TemperaturApp />
     </div>
   );
 }
 
-function CardApp() {
+function SauerstoffApp() {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
+        <h2 className="card_title">Sauerstoff</h2>
+        <p className="card__description">Zeigt den Sauerstoffgehalt an!</p>
+      </div>
+      <button className="card__btn">Zeig</button>
+    </div>
+  );
+}
+
+function LautstaerkeApp() {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
+        <h2 className="card_title">Lautstärke</h2>
+        <p className="card__description">Zeigt die Lautstärke an!</p>
+      </div>
+      <button className="card__btn">Zeig</button>
+    </div>
+  );
+}
+
+function TemperaturApp() {
   return (
     <div className="card">
       <div className="card__body">
         <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
         <h2 className="card_title">Temperatur</h2>
-        <p className="card__description">Zeigt die Temperatur an</p>
+        <p className="card__description">Zeigt die Temperatur an!</p>
       </div>
       <button className="card__btn">Zeig</button>
     </div>

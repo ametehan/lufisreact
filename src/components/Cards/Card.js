7c6525d18b041b5582db4660fe1edd8a4a8dfc33
Numan Erter <numan.erter@student.htldornbirn.at>
2022-05-11 14:42:07 +0200

    Ende Unterricht

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -7,6 +7,8 @@ function Card() {
       <SauerstoffApp />
       <LautstaerkeApp />
       <TemperaturApp />
+      <QualitaetApp />
+      <FeuchtigkeitApp />
     </div>
   );
 }
@@ -44,6 +46,32 @@ function TemperaturApp() {
         <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
         <h2 className="card_title">Temperatur</h2>
         <p className="card__description">Zeigt die Temperatur an!</p>
+      </div>
+      <button className="card__btn">Zeig</button>
+    </div>
+  );
+}
+
+function QualitaetApp() {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
+        <h2 className="card_title">Luftqualität</h2>
+        <p className="card__description">Zeigt die Luftqualität an!</p>
+      </div>
+      <button className="card__btn">Zeig</button>
+    </div>
+  );
+}
+
+function FeuchtigkeitApp() {
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src="https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8=" />
+        <h2 className="card_title">Luftfeuchtigkeit</h2>
+        <p className="card__description">Zeigt die Luftfeuchtigkeit an!</p>
       </div>
       <button className="card__btn">Zeig</button>
     </div>

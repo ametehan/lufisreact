8cdcf5f20d30847a7bfcca0e3a7cc0ebfbf06118
metehan <metehan.altundal@student.htldornbirn.at>
2022-05-18 14:20:35 +0200

    og

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -48,7 +48,7 @@ function Card() {
             image={
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
-            name="Feuchtigkeit"
+            name="Luftqualität"
             desc="ein og sauerstoff og move"
           />
         </Grid>

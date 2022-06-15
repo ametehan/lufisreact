7292545502ba1a8ed2fd4ac086567e57b42a1786
kristijan <kristijan.mladenovic@student.htldornbirn.at>
2022-06-15 14:20:36 +0200

    og text 2

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -13,7 +13,7 @@ function Card() {
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
             name="Oxygen"
-            desc="Shows the current CO² status in the room"
+            desc="Shows the current CO2 status"
           />
         </Grid>
         <Grid item xs={10} md={4} lg={2.4}>
@@ -22,7 +22,7 @@ function Card() {
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
             name="Sound"
-            desc="Shows how loud it is in the room"
+            desc="Shows how loud it is "
           />
         </Grid>
         <Grid item xs={10} md={4} lg={2.4}>
@@ -31,7 +31,7 @@ function Card() {
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
             name="Temperature"
-            desc="Shows the current temperature in the room"
+            desc="Shows the current temperature"
           />
         </Grid>
         <Grid item xs={10} md={4} lg={2.4}>
@@ -40,7 +40,7 @@ function Card() {
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
             name="Humidity"
-            desc="Shows the humidity in the room"
+            desc="Shows the humidity"
           />
         </Grid>
         <Grid item xs={10} md={4} lg={2.4}>
@@ -49,7 +49,7 @@ function Card() {
               "https://media.istockphoto.com/vectors/thermometer-icon-hot-temperature-vector-vector-id852156476?b=1&k=20&m=852156476&s=170667a&w=0&h=9zZF4XN91zH_bq_eVvDC-LEJhGx7c1AMZMikjnvAXT8="
             }
             name="Air quality"
-            desc="Shows how fresh the air is in the room"
+            desc="Shows how fresh the air is"
           />
         </Grid>
       </Grid>
